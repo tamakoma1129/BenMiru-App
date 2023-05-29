b887ce5f40a2b60e4e0d79c004e7371bf3986525
tamakoma <tamakoma0125@gmail.com>
2023-05-30 01:27:07 +0900

    [fix]　ストップウォッチ画面のレイアウトの調整
    機種によってはみ出ることがあったので、相対的に綺麗に表示されるようにレイアウトを調整。
    殆どChatGPTにレイアウトを調整してもらったが、やってることは相対的に位置と大きさを変更しているだけ。

--- StudyRecordNonChatGPT/StopwatchView.swift
+++ StudyRecordNonChatGPT/StopwatchView.swift
@@ -7,12 +7,12 @@ import SwiftUI
  バックからフォアグラウンドに戻ると現在時刻が取得され、Userdefaultに保存された時間との差分をタイマーに加算し、タイマーが再開する。
  */
 struct StopwatchView: View {
-    @Binding var allMinuteTime : Int
-    @Binding var uiColor : UIColor
-    @Binding var genreId : String
+    @Binding var allMinuteTime: Int
+    @Binding var uiColor: UIColor
+    @Binding var genreId: String
     @StateObject private var viewModel: StopwatchViewModel
-    //関数startが起動するたびにallMinuteTimeにストップウォッチの時間が入る
-    init(allMinuteTime: Binding<Int>,uiColor: Binding<UIColor>,genreId: Binding<String>) {
+    
+    init(allMinuteTime: Binding<Int>, uiColor: Binding<UIColor>, genreId: Binding<String>) {
         _allMinuteTime = allMinuteTime
         _uiColor = uiColor
         _genreId = genreId
@@ -20,62 +20,71 @@ struct StopwatchView: View {
     }
 
     var body: some View {
-        VStack(spacing:30) {
-            ZStack {
-                //四角を作って
-                Rectangle()
-                    .fill(Color.white)
-                    .frame(width: 200, height: 200)
-                    .border(Color.black, width: 1)
-                //その中を斜線で埋める
-                EasyBlockModel()
-                    .stroke(Color(uiColor), lineWidth: 1)
-                    .frame(width: 200, height: 200)
-                    .clipped()
-            }
-            //経過時間を表示
-            Text(viewModel.displayTime)
-                .font(.system(size: 80)).fontWeight(.thin)    //相対サイズでないので、他の方法の検討
-                .padding()
-            HStack(spacing:50) {
-                //リセット
-                Button(action: { viewModel.reset() }) {
-                    Text("Reset")
-                    .padding()
-                    .frame(width: 100, height: 100) //円の大きさ
-                    .foregroundColor(Color.black)   //文字の色
-                    .overlay(
-                        Circle()    //円に指定
-                            .stroke(Color.black, lineWidth: 1)
-                    )
-                }
-                //開始・一時停止  isRunningで条件分岐
-                Button(action: {
-                    if viewModel.isRunning {
-                        viewModel.stop()
-                    } else {
-                        viewModel.start()
+        GeometryReader { geometry in
+            HStack {
+                Spacer()
+                VStack(spacing: geometry.size.height * 0.03) {
+                    Spacer()
+                    ZStack {
+                        //四角を作って
+                        Rectangle()
+                            .fill(Color.white)
+                            .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.6)
+                            .border(Color.black, width: 1)
+                        //その中を斜線で埋める
+                        EasyBlockModel()
+                            .stroke(Color(uiColor), lineWidth: 1)
+                            .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.6)
+                            .clipped()
                     }
-                }) {
-                    Text(viewModel.isRunning ? "一時停止" : "開始")
-                    .padding()
-                    .frame(width: 100, height: 100) //円の大きさ
-                    .foregroundColor(Color.black)   //文字の色
-                    .overlay(
-                        Circle()    //円に指定
-                            .stroke(viewModel.isRunning ? Color.red : Color(uiColor), lineWidth: 2)
-                    )
+                    //経過時間を表示
+                    Text(viewModel.displayTime)
+                        .font(.system(size: geometry.size.width * 0.2)).fontWeight(.thin)
+                        .padding()
+                    HStack(spacing: geometry.size.width * 0.1) {
+                        //リセット
+                        Button(action: { viewModel.reset() }) {
+                            Text("Reset")
+                            .padding()
+                            .frame(width: geometry.size.width * 0.25, height: geometry.size.width * 0.25) //円の大きさ
+                            .foregroundColor(Color.black)   //文字の色
+                            .overlay(
+                                Circle()    //円に指定
+                                    .stroke(Color.black, lineWidth: 1)
+                            )
+                        }
+                        //開始・一時停止  isRunningで条件分岐
+                        Button(action: {
+                            if viewModel.isRunning {
+                                viewModel.stop()
+                            } else {
+                                viewModel.start()
+                            }
+                        }) {
+                            Text(viewModel.isRunning ? "一時停止" : "開始")
+                            .padding()
+                            .frame(width: geometry.size.width * 0.25, height: geometry.size.width * 0.25) //円の大きさ
+                            .foregroundColor(Color.black)   //文字の色
+                            .overlay(
+                                Circle()    //円に指定
+                                    .stroke(viewModel.isRunning ? Color.red : Color(uiColor), lineWidth: 2)
+                            )
+                        }
+                    }
+                    Spacer()
+                }//UIApplication.willResignActiveNotificationがバックグラウンドに移る際に出る通知。それを受け取ってviewModel.appMovedToBackground()メソッドを呼んでる。
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                    viewModel.appMovedToBackground()
+                }//こちらは上のフォアグラウンドに戻ってきた時ver
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                    viewModel.appMovedToForeground()
                 }
+                Spacer()
             }
-        }//UIApplication.willResignActiveNotificationがバックグラウンドに移る際に出る通知。それを受け取ってviewModel.appMovedToBackground()メソッドを呼んでる。
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
-            viewModel.appMovedToBackground()
-        }//こちらは上のフォアグラウンドに戻ってきた時ver
-        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
-            viewModel.appMovedToForeground()
         }
     }
 }
+
 //ストップウォッチの機能を追加するためのクラス
 class StopwatchViewModel: ObservableObject {
     @Published var displayTime: String = "00:00:00"
